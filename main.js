73386ade802a4c9fcda784fb3f057a4eaf80691b
felipeab03 <felipeab03@gmail.com>
2020-05-27 10:37:34 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+class List{
+    constructor(){
+        this.data=[];
+    }
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+class TodoList extends List{
+    
+}
+
+const MinhaLista = new TodoList;
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo');
+}
